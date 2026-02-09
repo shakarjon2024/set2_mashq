a91d28156763053e2d53837a07a3b139f8692127
shakarjon2024 <rozibayevashakarjon1707@gmail.com>
2026-02-09 22:55:18 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+t = {1, 2}
+qoshish = [3, 4, 3, 5]
+t.update(qoshish)
+print(t)  # {1, 2, 3, 4, 5}
+
+
+royxat = [7, 3, 7, 9, 3, 1]
+takrorlarsiz = list(set(royxat))
+print(takrorlarsiz)  # [7, 3, 9, 1] tartib o‘zgarishi mumkin
+
+
+
+matn = "salom dunyo salom hammaga"
+sozlar = set(matn.split())
+print(len(sozlar))  # 3
+
+
+
+
